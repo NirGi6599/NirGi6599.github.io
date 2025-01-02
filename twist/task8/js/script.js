43f1fa38c7f91892b4930414954878ab60780682
NirGi6599 <nirn127@gmail.com>
2025-01-02 13:31:46 +0200

    task8

--- twist/task8/js/script.js
+++ twist/task8/js/script.js
@@ -1,5 +1,16 @@
 // JavaScript source code
+function backtostart() {
+   document.getElementById("dorothy").style.left = "0px";
+   document.getElementById("dorothy").style.top = "0px";
+   document.getElementById("rs").style.display = "none";
+
+}
+
+
 function divClicked() {
-	x = document.getelementbyid("dorothy").style.left
-   document.getelementbyid("dorothy").style.left = x + 300px; 
+   document.getElementById("dorothy").style.left = "300px";
+   document.getElementById("dorothy").style.top = "400px";
+   document.getElementById("rs").style.display = "inline";
+   
+   
 }
