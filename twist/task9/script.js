ee51cbaf905d488396bfa9758be7dc41bb04e529
NirGi6599 <nirn127@gmail.com>
2025-01-18 06:48:59 +0200

    task9

--- twist/task9/script.js
+++ twist/task9/script.js
@@ -4,33 +4,55 @@
 function Plus(){
 var x =document.getElementById("num1").value;
 var y = document.getElementById("num2").value;
-var res = document.getElementById("res").innerHTML;
+var res = document.getElementById("res");
 x = Number(x);
 y = Number(y);
-res.innerText = (x+y);
+if (!isNaN(x) && !isNaN(y)){
+res.innerText = (x+y);}
+else{
+    alert ("You can only write numbers, Wiseguy")
+}
 }
 
 
 function Minus(){
 var x =document.getElementById("num1").value;
 var y = document.getElementById("num2").value;
+var res = document.getElementById("res");
 x = Number(x);
 y = Number(y);
-alert (x-y);
+if (!isNaN(x) && !isNaN(y)){
+    res.innerText = (x-y);}
+else{
+        alert ("You can only write numbers, Wiseguy")
+}
 }
 
 function Multi(){
 var x =document.getElementById("num1").value;
 var y = document.getElementById("num2").value;
+var res = document.getElementById("res");
 x = Number(x);
 y = Number(y);
-alert (x*y);
+if (!isNaN(x) && !isNaN(y)){
+    res.innerText = (x*y);}
+else{
+        alert ("You can only write numbers, Wiseguy")
+}
 }
 
 function Divide(){
 var x =document.getElementById("num1").value;
 var y = document.getElementById("num2").value;
+var res = document.getElementById("res");
 x = Number(x);
 y = Number(y);
-alert (x/y);
+if (y==0){
+    alert ("Dont you dare divide by 0, you imbecile") 
+}
+else if (!isNaN(x) && !isNaN(y)){
+    res.innerText = (x/y);}
+    else{
+        alert ("You can only write numbers, Wiseguy")
+}
 }
